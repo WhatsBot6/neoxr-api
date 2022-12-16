a11b558e4f55d5c75555f4a3040ec6283b356ab4
neoxr <neoxr@null.net>
2022-12-16 08:44:35 +0700

    update

--- routes/api.js
+++ routes/api.js
@@ -1,60 +1,56 @@
-let express = require('express'), router = express.Router(), yt = require('../lib/yt')
+const express = require('express'),
+   router = express.Router(),
+   yt = new(require('../lib/yt'))
 
 router.get('/music', async (req, res) => {
-	let q = req.query.q
-	let apikey = req.query.apikey
-	if (!q) return res.json(global.status.query)
-	if (!apikey) return res.json(global.status.apikey)
-	if (!global.apikey.includes(apikey)) return res.json(global.status.invalidKey)
-	let result = await yt.music(q)
-	res.header('Content-Type: application/json')
-	res.type('json').send(JSON.stringify(result, null, 2))
+   let q = req.query.q
+   if (!q) return res.json(global.status.query)
+   let result = await yt.audio(q)
+   res.header('Content-Type: application/json')
+   res.type('json').send(JSON.stringify(result, null, 2))
 })
 
 router.get('/video', async (req, res) => {
-	let q = req.query.q
-	let apikey = req.query.apikey
-	if(!q) return res.json(global.status.query)
-	if (!apikey) return res.json(global.status.apikey)
-	if (!global.apikey.includes(apikey)) return res.json(global.status.invalidKey)
-	let result = await yt.video(q)
-	res.header('Content-Type: application/json')
-	res.type('json').send(JSON.stringify(result, null, 2))
+   let q = req.query.q
+   if (!q) return res.json(global.status.query)
+   let result = await yt.video(q)
+   res.header('Content-Type: application/json')
+   res.type('json').send(JSON.stringify(result, null, 2))
+})
+
+router.get('/analyze', async (req, res) => {
+   let url = req.query.url
+   if (!url) return res.json(global.status.url)
+   if (!url.match('youtu.be') && !url.match('youtube.com')) return res.json(global.status.invalidURL)
+   let result = await yt.analyze(url)
+   res.header('Content-Type: application/json')
+   res.type('json').send(JSON.stringify(result, null, 2))
 })
 
 router.get('/yta', async (req, res) => {
-	let url = req.query.url
-	let apikey = req.query.apikey
-	if (!url) return res.json(global.status.url)
-	if (!url.match('youtu.be') && !url.match('youtube.com')) return res.json(global.status.invalidURL)
-	if (!apikey) return res.json(global.status.apikey)
-	if (!global.apikey.includes(apikey)) return res.json(global.status.invalidKey)
-	let result = await yt.ytmp3(url)
-	res.header('Content-Type: application/json')
-	res.type('json').send(JSON.stringify(result, null, 2))
+   let url = req.query.url
+   if (!url) return res.json(global.status.url)
+   if (!url.match('youtu.be') && !url.match('youtube.com')) return res.json(global.status.invalidURL)
+   let result = await yt.yta(url)
+   res.header('Content-Type: application/json')
+   res.type('json').send(JSON.stringify(result, null, 2))
 })
 
 router.get('/ytv', async (req, res) => {
-	let url = req.query.url
-	let apikey = req.query.apikey
-	if (!url) return res.json(global.status.url)
-	if (!url.match('youtu.be') && !url.match('youtube.com')) return res.json(global.status.invalidURL)
-	if (!apikey) return res.json(global.status.apikey)
-	if (!global.apikey.includes(apikey)) return res.json(global.status.invalidKey)
-	let result = await yt.ytmp4(url)
-	res.header('Content-Type: application/json')
-	res.type('json').send(JSON.stringify(result, null, 2))
+   let { url, quality }} = req.query
+   if (!url) return res.json(global.status.url)
+   if (!url.match('youtu.be') && !url.match('youtube.com')) return res.json(global.status.invalidURL)
+   let result = await yt.ytv(url, quality)
+   res.header('Content-Type: application/json')
+   res.type('json').send(JSON.stringify(result, null, 2))
 })
 
 router.get('/yts', async (req, res) => {
-	let q = req.query.q
-	let apikey = req.query.apikey
-	if(!q) return res.json(handle.query)
-	if (!apikey) return res.json(global.status.apikey)
-	if (!global.apikey.includes(apikey)) return res.json(global.status.invalidKey)
-	let result = await yt.search(q)
-	res.header('Content-Type: application/json')
-	res.type('json').send(JSON.stringify(result, null, 2))
+   let q = req.query.q
+   if (!q) return res.json(handle.query)
+   let result = await yt.search(q)
+   res.header('Content-Type: application/json')
+   res.type('json').send(JSON.stringify(result, null, 2))
 })
 
 module.exports = router
